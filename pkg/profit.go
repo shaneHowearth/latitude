d593f914487fc5f40dae7658d47ba6ee0a17b726
Shane Howearth <Shane.h.1@gmail.com>
2019-05-26 10:30:31 +1000

    Add GetMaxProfit

--- pkg/profit.go
+++ pkg/profit.go
@@ -1,7 +1,32 @@
 package latitude
 
-// LocalProfit - max profit available in the given slice
-func LocalProfit(vals []int) (min, max, profit int) {
+// OFFSET - Array index offset
+const OFFSET = 1
+
+// GetMaxProfit -
+func GetMaxProfit(vals []int) (min, max, profit int) {
+	min, max, profit = GetLocalProfit(vals)
+	if min == -1 || max == -1 {
+		return -1, -1, 0
+	}
+	for i := max + OFFSET; i < len(vals)-1; {
+		localMin, localMax, localProfit := GetLocalProfit(vals[i:])
+		if localProfit > profit {
+			min, max, profit = max+localMin+OFFSET, max+localMax+OFFSET, localProfit
+		}
+		if localMax == -1 {
+			break
+		}
+		// Update i here because we want to stop this loop
+		// at the correct index in the vals slice.
+		// But it is not a uniform increment.
+		i += localMax
+	}
+	return min, max, profit
+}
+
+// GetLocalProfit - max profit available in the given slice
+func GetLocalProfit(vals []int) (min, max, profit int) {
 	max = FindMax(vals)
 	if max != -1 {
 		min = FindMin(vals[0:max])
--- pkg/profit_test.go
+++ pkg/profit_test.go
@@ -7,12 +7,76 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLocalProfit(t *testing.T) {
+func TestGetMaxProfit(t *testing.T) {
 	testcases := map[string]struct {
 		inputVals []int
 		min       int
 		max       int
 		profit    int
+	}{
+		"Happy Path": {
+			inputVals: []int{1, 2, 3, 4, 5},
+			min:       0,
+			max:       4,
+			profit:    4,
+		},
+		"No result": {
+			inputVals: []int{9, 2, 3, 4, 5},
+			min:       -1,
+			max:       -1,
+			profit:    0,
+		},
+		"First Max": {
+			inputVals: []int{4, 5, 3, 4, 5},
+			min:       2,
+			max:       4,
+			profit:    2,
+		},
+		"Long Input": {
+			inputVals: []int{54, 40, 47, 88, 37, 81, 32, 67, 56, 93,
+				18, 16, 12, 95, 96, 17, 11, 4, 16, 66,
+				51, 18, 6, 56, 16, 31, 38, 49, 6, 15,
+				80, 65, 57, 70, 43, 7, 21, 12, 62, 72,
+				53, 51, 94, 90, 4, 73, 29, 14, 72, 32,
+				72, 63, 85, 48, 49, 18, 11, 60, 10, 89,
+				16, 75, 16, 61, 51, 28, 83, 76, 42, 3,
+				37, 53, 96, 49, 95, 1, 94, 60, 94, 66, 89,
+				4, 27, 70, 25, 53, 14, 83, 14, 85, 26,
+				25, 92, 22, 56, 47, 38, 73, 95, 71, 49},
+			min:    69,
+			max:    72,
+			profit: 93,
+		},
+		"Min further inside": {
+			inputVals: []int{5, 2, 3, 4, 9, 3},
+			min:       1,
+			max:       4,
+			profit:    7,
+		},
+		"Empty list": {
+			inputVals: []int{},
+			min:       -1,
+			max:       -1,
+			profit:    0,
+		},
+	}
+
+	for name, tc := range testcases {
+		minOut, maxOut, profitOut := latitude.GetMaxProfit(tc.inputVals)
+		assert.Equal(t, tc.min, minOut, "Output produced incorrect min index for %s", name)
+		assert.Equal(t, tc.max, maxOut, "Output produced incorrect max index for %s", name)
+		assert.Equal(t, tc.profit, profitOut, "Output produced incorrect profit for %s", name)
+	}
+
+}
+
+func TestGetLocalProfit(t *testing.T) {
+	testcases := map[string]struct {
+		inputVals []int
+		min       int
+		max       int
+		profit    int
+		err       error
 	}{
 		"Happy Path": {
 			inputVals: []int{1, 2, 3, 4, 5},
@@ -49,7 +113,7 @@ func TestLocalProfit(t *testing.T) {
 	}
 
 	for name, tc := range testcases {
-		minOut, maxOut, profitOut := latitude.LocalProfit(tc.inputVals)
+		minOut, maxOut, profitOut := latitude.GetLocalProfit(tc.inputVals)
 		assert.Equal(t, tc.min, minOut, "Output produced incorrect min index for %s", name)
 		assert.Equal(t, tc.max, maxOut, "Output produced incorrect max index for %s", name)
 		assert.Equal(t, tc.profit, profitOut, "Output produced incorrect profit for %s", name)
